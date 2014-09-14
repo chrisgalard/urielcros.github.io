5a68e6c201c3d06414e838b3d53b6f8261499317
Christian Cisneros <uriel.cisne@gmail.com>
2014-09-13 23:57:55 -0600

    Initial commit

--- js/custom.js
+++ js/custom.js
@@ -6,11 +6,26 @@ $(document).ready(function() {
             $('.go-top').fadeOut('slow');
         }
     });
-    
+
     $('.go-top').click(function() {
         $('html, body').animate({
             scrollTop: 0
-        }, 800);
+        }, 2000, 'easeOutBounce');
         return false;
     });
+
+    var hello = $('.me span:first-child');
+    var info = $('.me span:last-child');
+
+    hello.css('position', 'relative');
+    info.css('position', 'relative');
+    hello.css('right', '1500px');
+    info.css('left', '1500px');
+
+    hello.animate({
+        right: 0
+    }, 2000, 'easeOutElastic');
+    info.animate({
+        left: 0
+    }, 2000, 'easeOutElastic');
 });
